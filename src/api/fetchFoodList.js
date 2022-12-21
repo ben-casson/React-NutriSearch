31a8a58f8d18060e3250fd3e770772e8e0d93f02
Ben Casson <bcasson11@gmail.com>
2022-12-21 10:54:12 -0600

    Export array of fetched food results

--- src/api/fetchFoodList.js
+++ src/api/fetchFoodList.js
@@ -1,3 +1,5 @@
+export let foodResults;
+
 export default function fetchFood(foodItem) {
     fetch(
         `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=lWLhwUp3ZfGng76exaxb8ddTvr5SlfSs9G8wk3b9&query=${foodItem}`
@@ -5,6 +7,8 @@ export default function fetchFood(foodItem) {
         .then((response) => response.json())
         .then((foodData) => {
             console.log(foodData);
+            foodResults = foodData.foods;
+            console.log(foodResults)
         })
         .catch((error) => {
             console.error(`An error occurred: ${error}`);
