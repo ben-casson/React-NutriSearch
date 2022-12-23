8d07ffe6704a7d1da29a000adcf8d39139c51493
Ben Casson <bcasson11@gmail.com>
2022-12-22 18:17:50 -0600

    Prevent old FoodPage from temporarily loading

--- src/Search.jsx
+++ src/Search.jsx
@@ -11,10 +11,14 @@ export default function Search({
     foodIsSelected,
     setFoodIsSelected,
     fetchFoodList,
+    setSelectedFoodDetails,
 }) {
     function handleFetchFood(e) {
         //prevents form from refreshing the page
         e.preventDefault();
+        //prevents previous FoodPage from temporarily loading before current FoodPage loads
+        setSelectedFoodDetails({});
+        // setFoodList([])
         fetchFoodList(food);
         if (foodIsSelected) {
             setFoodIsSelected(!foodIsSelected);
@@ -25,12 +29,12 @@ export default function Search({
         <header>
             <div id='search-form-container'>
                 <form onSubmit={handleFetchFood}>
-                    <label htmlFor='food-input'>Search</label>
+                    <label htmlFor='food-input' id='food-input-label'></label>
                     <div id='search-bar-and-btn-container'>
                         <input
                             type='text'
                             value={food}
-                            placeholder="apple"
+                            placeholder='Search by food name...'
                             id='food-input'
                             onChange={(e) => setFood(e.target.value)}
                         />
