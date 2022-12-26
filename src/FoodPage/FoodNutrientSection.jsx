636d969ee63f849b48b5f54eae872e3d387ab287
Ben Casson <bcasson11@gmail.com>
2022-12-26 14:14:34 -0600

    Pass portionAmount prop to child

--- src/FoodPage/FoodNutrientSection.jsx
+++ src/FoodPage/FoodNutrientSection.jsx
@@ -1,16 +1,16 @@
 import NutrientRow from './NutrientRow';
 
-export default function FoodNutrientSection({ foodDetails }) {
+export default function FoodNutrientSection({ foodDetails, portionAmount }) {
     return (
         <section id='food-nutrients-table-section'>
             <div id='food-nutrients-table-title-container'>
                 <p id='food-nutrients-title-name'>Name</p>
                 <p id='food-nutrients-title-amount'>Amount</p>
-                <p id='food-nutrients-title-daily-value'>% Daily Value</p>
+                <p id='food-nutrients-title-daily-value'>Daily Value</p>
             </div>
             <div id='food-nutrients-table-content-container'>
                 {Object.keys(foodDetails).length != 0 && (
-                    <NutrientRow nutrientsArray={foodDetails.foodNutrients} />
+                    <NutrientRow nutrientsArray={foodDetails.foodNutrients} foodDetails={foodDetails} portionAmount={portionAmount} />
                 )}
             </div>
         </section>
