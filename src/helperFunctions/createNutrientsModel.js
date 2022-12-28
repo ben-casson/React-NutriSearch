60dce6fb86d9bbfc5c359d76daba157d56c331e9
Ben Casson <bcasson11@gmail.com>
2022-12-27 19:07:10 -0600

    Add functions to create a Map of nutrient info

---
+++ src/helperFunctions/createNutrientsModel.js
@@ -0,0 +1,38 @@
+function createNutrientObject(
+    nutrientArray,
+    tempNutrientModel,
+    nutrientNameFromAPI,
+    type,
+    unit,
+    dailyValue
+) {
+    const tempNutrientObj = {};
+    for (let nutrientObj of nutrientArray) {
+        if (
+            nutrientObj.nutrient.name == nutrientNameFromAPI &&
+            nutrientObj.nutrient.unitName == unit
+        ) {
+            tempNutrientObj.type = type;
+            tempNutrientObj.amount = nutrientObj.amount;
+            tempNutrientObj.dailyValue = dailyValue;
+            tempNutrientObj.unit = unit;
+            break;
+        } else {
+            tempNutrientObj.type = type;
+            tempNutrientObj.amount = '-';
+            tempNutrientObj.dailyValue = '-';
+            tempNutrientObj.unit = unit;
+        }
+    }
+    let keyName = type;
+    tempNutrientModel.set(keyName, tempNutrientObj);
+}
+
+export function createNutrientsModel(nutrientArray) {
+    const tempNutrientModel = new Map();
+    createNutrientObject(nutrientArray, tempNutrientModel, 'Energy', 'Energy', 'kcal', 2000);
+    createNutrientObject(nutrientArray, tempNutrientModel, 'Water', 'Water', 'g', 3500);
+    createNutrientObject(nutrientArray, tempNutrientModel, 'Caffeine', 'Caffeine', 'mg', 0);
+    createNutrientObject(nutrientArray, tempNutrientModel, 'Alcohol, ethyl', 'Alcohol', 'g', 0);
+    return tempNutrientModel;
+}
