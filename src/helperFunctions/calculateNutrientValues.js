8b3622ffc6ecca665f0283d6154502889a0f032b
Ben Casson <bcasson11@gmail.com>
2022-12-27 19:05:27 -0600

    Add functions to calculate nutrient values

---
+++ src/helperFunctions/calculateNutrientValues.js
@@ -0,0 +1,22 @@
+export function calculateNutrientAmount(portionQuantity, nutrientModel, key) {
+    let amount = portionQuantity
+        ? Math.round(nutrientModel.get(`${key}`).amount * portionQuantity) / 100
+        : nutrientModel.get(`${key}`).amount;
+
+    let unit = nutrientModel.get(`${key}`).unit;
+
+    return (amount >= 0 ? amount : '-') + ' ' + unit;
+}
+
+export function calculateNutrientDailyValue(portionQuantity, nutrientModel, key) {
+    let dailyValue = portionQuantity
+        ? Math.round(
+              (nutrientModel.get(`${key}`).amount * portionQuantity * 10) /
+                  nutrientModel.get(`${key}`).dailyValue
+          ) / 10
+        : Math.round(
+              (nutrientModel.get(`${key}`).amount / nutrientModel.get(`${key}`).dailyValue) * 1000
+          ) / 10;
+
+    return (dailyValue >= 0  && dailyValue != Infinity) ? dailyValue + '%' : '-';
+}
