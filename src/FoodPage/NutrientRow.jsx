3af6c61b4475a61054dee66fa728168e4b827c28
Ben Casson <bcasson11@gmail.com>
2022-12-27 19:04:46 -0600

    Reformat

--- src/FoodPage/NutrientRow.jsx
+++ src/FoodPage/NutrientRow.jsx
@@ -4,11 +4,7 @@ import './NutrientRow.css';
 
 export default function NutrientRow({ name, amount, dailyValue, background }) {
     return (
-        <div
-            className={
-                'nutrient-row' + (background == 'dark' && ' dark-background')
-            }
-        >
+        <div className={'nutrient-row' + (background == 'dark' ? ' dark-background' : '')}>
             <p className='nutrient-name'>{name}</p>
             <p className='nutrient-amount'>{amount}</p>
             <p className='nutrient-daily-value'>{dailyValue}</p>
