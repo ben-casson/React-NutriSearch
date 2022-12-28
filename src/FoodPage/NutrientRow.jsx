fef78098a4ee53b0287ef8d894dd31cc0c597b77
Ben Casson <bcasson11@gmail.com>
2022-12-28 16:11:16 -0600

    Add new className for nested rows

--- src/FoodPage/NutrientRow.jsx
+++ src/FoodPage/NutrientRow.jsx
@@ -1,9 +1,9 @@
 import './NutrientRow.css';
 
-export default function NutrientRow({ name, amount, dailyValue, background }) {
+export default function NutrientRow({ name, amount, dailyValue, background, nested }) {
     return (
         <div className={'nutrient-row' + (background == 'dark' ? ' dark-background' : '')}>
-            <p className='nutrient-name'>{name}</p>
+            <p className={'nutrient-name' + (nested == 'nested' ? ' nested' : '')}>{name}</p>
             <p className='nutrient-amount'>{amount}</p>
             <p className='nutrient-daily-value'>{dailyValue}</p>
         </div>
