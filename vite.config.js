820d36501f126a29fa6aa8e4165b4dc4bc096615
Ben Casson <bcasson11@gmail.com>
2022-12-30 10:39:35 -0600

    Update url

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //uncomment base for deploy
-  base: "/food-nutrition-search/",
+  base: "/React-NutriSearch/",
 })
