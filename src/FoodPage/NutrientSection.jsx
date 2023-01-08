39660cee487dc5268515e2a277b6daba1373e92f
Ben Casson <bcasson11@gmail.com>
2023-01-08 14:01:10 -0600

    Edit implementation of conditional rendering

--- src/FoodPage/NutrientSection.jsx
+++ src/FoodPage/NutrientSection.jsx
@@ -39,62 +39,64 @@ export default function NutrientSection({ foodDetails, portionAmount }) {
                 <p id='food-nutrients-title-amount'>Amount</p>
                 <p id='food-nutrients-title-daily-value'>Daily Value</p>
             </div>
-            <div id='food-nutrients-table-content-container'>
-                <div className='nutrient-row'>
-                    <p className='nutrient-title'>General</p>
+            {nutrientModel.size != 0 && (
+                <div id='food-nutrients-table-content-container'>
+                    <div className='nutrient-row'>
+                        <p className='nutrient-title'>General</p>
+                    </div>
+                    {createNutrientRow('Energy', 'dark')}
+                    {createNutrientRow('Water', 'light')}
+                    {createNutrientRow('Caffeine', 'dark')}
+                    {createNutrientRow('Alcohol', 'light')}
+                    <div className='nutrient-row dark-background'>
+                        <p className='nutrient-title'>Carbohydrates</p>
+                    </div>
+                    {createNutrientRow('Carbs', 'light')}
+                    {createNutrientRow('Sugars', 'dark', 'nested')}
+                    {createNutrientRow('Added Sugars', 'light', 'nested')}
+                    {createNutrientRow('Fiber', 'dark', 'nested')}
+                    <div className='nutrient-row'>
+                        <p className='nutrient-title'>Lipids</p>
+                    </div>
+                    {createNutrientRow('Fat', 'dark')}
+                    {createNutrientRow('Monounsaturated', 'light', 'nested')}
+                    {createNutrientRow('Polyunsaturated', 'dark', 'nested')}
+                    {createNutrientRow('Saturated', 'light', 'nested')}
+                    {createNutrientRow('Trans-Fats', 'dark', 'nested')}
+                    {createNutrientRow('Cholesterol', 'light')}
+                    <div className='nutrient-row dark-background'>
+                        <p className='nutrient-title'>Protein</p>
+                    </div>
+                    {createNutrientRow('Protein', 'light')}
+                    <div className='nutrient-row dark-background'>
+                        <p className='nutrient-title'>Vitamins</p>
+                    </div>
+                    {createNutrientRow('B1 (Thiamin)', 'light')}
+                    {createNutrientRow('B2 (Riboflavin)', 'dark')}
+                    {createNutrientRow('B3 (Niacin)', 'light')}
+                    {createNutrientRow('B5 (Pantothenic Acid)', 'dark')}
+                    {createNutrientRow('B6 (Pyridoxine)', 'light')}
+                    {createNutrientRow('B12 (Cobalamin)', 'dark')}
+                    {createNutrientRow('Folate', 'light')}
+                    {createNutrientRow('Vitamin A', 'dark')}
+                    {createNutrientRow('Vitamin C', 'light')}
+                    {createNutrientRow('Vitamin D', 'dark')}
+                    {createNutrientRow('Vitamin E', 'light')}
+                    {createNutrientRow('Vitamin K', 'dark')}
+                    <div className='nutrient-row'>
+                        <p className='nutrient-title'>Minerals</p>
+                    </div>
+                    {createNutrientRow('Calcium', 'dark')}
+                    {createNutrientRow('Copper', 'light')}
+                    {createNutrientRow('Iron', 'dark')}
+                    {createNutrientRow('Magnesium', 'light')}
+                    {createNutrientRow('Phosphorus', 'dark')}
+                    {createNutrientRow('Potassium', 'light')}
+                    {createNutrientRow('Selenium', 'dark')}
+                    {createNutrientRow('Sodium', 'light')}
+                    {createNutrientRow('Zinc', 'dark')}
                 </div>
-                {nutrientModel.size != 0 && createNutrientRow('Energy', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Water', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Caffeine', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Alcohol', 'light')}
-                <div className='nutrient-row dark-background'>
-                    <p className='nutrient-title'>Carbohydrates</p>
-                </div>
-                {nutrientModel.size != 0 && createNutrientRow('Carbs', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Sugars', 'dark', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Added Sugars', 'light', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Fiber', 'dark', 'nested')}
-                <div className='nutrient-row'>
-                    <p className='nutrient-title'>Lipids</p>
-                </div>
-                {nutrientModel.size != 0 && createNutrientRow('Fat', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Monounsaturated', 'light', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Polyunsaturated', 'dark', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Saturated', 'light', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Trans-Fats', 'dark', 'nested')}
-                {nutrientModel.size != 0 && createNutrientRow('Cholesterol', 'light')}
-                <div className='nutrient-row dark-background'>
-                    <p className='nutrient-title'>Protein</p>
-                </div>
-                {nutrientModel.size != 0 && createNutrientRow('Protein', 'light')}
-                <div className='nutrient-row dark-background'>
-                    <p className='nutrient-title'>Vitamins</p>
-                </div>
-                {nutrientModel.size != 0 && createNutrientRow('B1 (Thiamin)', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('B2 (Riboflavin)', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('B3 (Niacin)', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('B5 (Pantothenic Acid)', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('B6 (Pyridoxine)', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('B12 (Cobalamin)', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Folate', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Vitamin A', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Vitamin C', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Vitamin D', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Vitamin E', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Vitamin K', 'dark')}
-                <div className='nutrient-row'>
-                    <p className='nutrient-title'>Minerals</p>
-                </div>
-                {nutrientModel.size != 0 && createNutrientRow('Calcium', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Copper', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Iron', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Magnesium', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Phosphorus', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Potassium', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Selenium', 'dark')}
-                {nutrientModel.size != 0 && createNutrientRow('Sodium', 'light')}
-                {nutrientModel.size != 0 && createNutrientRow('Zinc', 'dark')}
-            </div>
+            )}
         </section>
     );
 }
