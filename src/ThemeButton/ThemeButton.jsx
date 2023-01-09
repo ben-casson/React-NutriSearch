5d97c755908ee1809b61562fef8577e93806e20c
Ben Casson <bcasson11@gmail.com>
2023-01-08 23:36:56 -0600

    Create theme button component

---
+++ src/ThemeButton/ThemeButton.jsx
@@ -0,0 +1,15 @@
+import './ThemeButton.css';
+
+export default function ThemeButton() {
+    function toggleTheme() {
+        if (document.body.className === 'dark') {
+            document.body.className = 'light';
+            document.querySelector('#theme-button').className = 'dark';
+        } else {
+            document.body.className = 'dark';
+            document.querySelector('#theme-button').className = 'light';
+        }
+    }
+
+    return <button id='theme-button' className='light' onClick={toggleTheme}></button>;
+}
