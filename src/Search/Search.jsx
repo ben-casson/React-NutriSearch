bd6c5db898e0e079465c76a0440c6311a59da16c
Ben Casson <bcasson11@gmail.com>
2023-01-08 23:36:21 -0600

    Remove header element

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -21,24 +21,22 @@ export default function Search({
     }
 
     return (
-        <header>
-            <div id='search-form-container'>
-                <form onSubmit={handleFetchFood}>
-                    <label htmlFor='food-input' id='food-input-label'></label>
-                    <div id='search-bar-and-btn-container'>
-                        <input
-                            type='text'
-                            value={food}
-                            placeholder='Search by food name...'
-                            id='food-input'
-                            onChange={(e) => {
-                                setFood(e.target.value);
-                            }}
-                        />
-                        <button id='search-btn' aria-label='Search'></button>
-                    </div>
-                </form>
-            </div>
-        </header>
+        <div id='search-form-container'>
+            <form onSubmit={handleFetchFood}>
+                <label htmlFor='food-input' id='food-input-label'></label>
+                <div id='search-bar-and-btn-container'>
+                    <input
+                        type='text'
+                        value={food}
+                        placeholder='Search by food name...'
+                        id='food-input'
+                        onChange={(e) => {
+                            setFood(e.target.value);
+                        }}
+                    />
+                    <button id='search-btn' aria-label='Search'></button>
+                </div>
+            </form>
+        </div>
     );
 }
